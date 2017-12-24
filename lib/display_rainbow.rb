ac1dfacf550050950d1c1e7838f7e643eb7b6f54
Stephane <son.trinh.dev@gmail.com>
2017-12-24 07:07:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  
+  puts "#{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}, #{colors[0][0].capitalize}: #{colors[0]}"
 end
 
 #colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
