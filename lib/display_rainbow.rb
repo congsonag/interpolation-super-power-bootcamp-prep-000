5627c141a2e78227356507ea5cf5632ce0ef8e95
Stephane <son.trinh.dev@gmail.com>
2017-12-24 07:02:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,7 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  string = ''
-  colors.each do |color|
-    string += "#{color[0].capitalize}: #{color}"
-    if position < colors.size - 1
-      string += ", "
-    end
-  end
-  print string
+  
 end
 
 #colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
